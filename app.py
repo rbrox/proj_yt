a4db97bbacfb1635a2abb7421b2514af01751c9a
rbrox <rishavsci@gmail.com>
2023-11-05 19:12:15 +0530

    data api working

---
+++ app.py
@@ -0,0 +1,18 @@
+import json
+from googleapiclient.discovery import build
+
+api_key = 'AIzaSyA08uOLNC85Y761i8tZw4EcaFyXhC7f4_c'
+
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+request = youtube.search().list(
+            part='snippet', 
+            maxResults=10, 
+            regionCode='US',
+            q='python'
+        )
+response = request.execute()
+
+print(json.dumps(response, indent=4))
+with open('response.json', 'w') as f:
+    f.write(json.dumps(response, indent=4))
